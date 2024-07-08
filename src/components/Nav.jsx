113e39116d2d90f038c689556ef34d6f31b1fb7b
gaurav basu <gauravbasu007@gmail.com>
2024-07-08 14:22:33 +0530

    [UPDATE] bug fix from landing page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,27 +16,30 @@ const Nav = ({ userName }) => {
       </NavLink>
       <div className="btn-group">
         {userName && (
-          <Form>
-          <NavLink to="/" className="btn btn--primary">
-            <span>Logout</span>
-            <LockClosedIcon width={20} />
-          </NavLink>
-          </Form>
+          <>
+            <Form>
+              <NavLink to="/" className="btn btn--primary">
+                <span>Logout</span>
+                <LockClosedIcon width={20} />
+              </NavLink>
+            </Form>
+
+            <Form
+              method="post"
+              action="logout"
+              onSubmit={(event) => {
+                if (!confirm("Delete user and all data?")) {
+                  event.preventDefault();
+                }
+              }}
+            >
+              <button type="submit" className="btn btn--warning">
+                <span>Delete User</span>
+                <TrashIcon width={20} />
+              </button>
+            </Form>
+          </>
         )}
-        <Form
-          method="post"
-          action="logout"
-          onSubmit={(event) => {
-            if (!confirm("Delete user and all data?")) {
-              event.preventDefault();
-            }
-          }}
-        >
-          <button type="submit" className="btn btn--warning">
-            <span>Delete User</span>
-            <TrashIcon width={20} />
-          </button>
-        </Form>
       </div>
     </nav>
   );
